56cc95dbf896fa98cd0e74cbb1f147a98ff5ae3c
Caleb Norton <n0603919@outlook.com>
2024-12-01 15:21:03 -0600

    Solve 2024 day 1

--- elixir/lib/advent_of_code/solution/year_2024/day_01.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_01.ex
@@ -1,7 +1,44 @@
 defmodule AdventOfCode.Solution.Year2024.Day01 do
-  def part1(_input) do
+  def part1(input) do
+    # split the input into two lists then convert the elements to integers and sort
+    [list1, list2] =
+      for list <-
+            input |> columns_to_lists(),
+          do: list |> Enum.map(&String.to_integer/1) |> Enum.sort()
+
+    # zip list and subtract the elements
+    Enum.zip(list1, list2)
+    |> Enum.map(fn {x, y} -> abs(x - y) end)
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    # split the input into two lists
+    [list1, list2] =
+      for list <-
+            input |> columns_to_lists(),
+          do: list |> Enum.map(&String.to_integer/1)
+
+    # convert list 2 into a map of occurrences
+    list2_map = Enum.reduce(list2, %{}, fn x, acc -> Map.update(acc, x, 1, &(&1 + 1)) end)
+    # for each element in list 1, see how may times it occurred in list2 then multiply
+    list1
+    |> Enum.reduce(0, fn x, acc -> acc + Map.get(list2_map, x, 0) * x end)
+  end
+
+  defp columns_to_lists(text) do
+    text
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line -> String.split(line, " ", trim: true) end)
+    |> transpose()
+  end
+
+  # https://stackoverflow.com/questions/5389254/transposing-a-2-dimensional-matrix-in-erlang
+  defp transpose([[] | _]) do
+    []
+  end
+
+  defp transpose(matrix) do
+    [Enum.map(matrix, &hd/1) | transpose(Enum.map(matrix, &tl/1))]
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_01_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_01_test.exs
@@ -6,21 +6,25 @@ defmodule AdventOfCode.Solution.Year2024.Day01Test do
   setup do
     [
       input: """
+      3   4
+      4   3
+      2   5
+      1   3
+      3   9
+      3   3
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
     result = part1(input)
 
-    assert result
+    assert result == 11
   end
 
-  @tag :skip
   test "part2", %{input: input} do
     result = part2(input)
 
-    assert result
+    assert result == 31
   end
 end
