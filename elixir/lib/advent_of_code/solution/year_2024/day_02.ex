251e35a18b7e0598a9f3a41fed8ea6bcb4ec9af1
Caleb Norton <n0603919@outlook.com>
2024-12-02 09:48:59 -0600

    Day 2 part 1

--- elixir/lib/advent_of_code/solution/year_2024/day_02.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_02.ex
@@ -1,7 +1,47 @@
 defmodule AdventOfCode.Solution.Year2024.Day02 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn line ->
+      line
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+    end)
   end
 
-  def part2(_input) do
+  def part1(parsed_input) do
+    parsed_input
+    |> Enum.map(&is_safe?/1)
+    |> Enum.count(&(&1))
   end
+
+  def part2(_parsed_input) do
+  end
+
+  defp process_pair(a, b) do
+    cond do
+      abs(a - b) > 3 -> :big_step
+      a == b -> :equal
+      a > b -> :decreasing
+      a < b -> :increasing
+    end
+  end
+
+  defp is_safe?(list, last \\ :begin)
+  defp is_safe?([head, next | rest], last) do
+    result = process_pair(head, next)
+    cond do
+      result == :big_step -> false
+      result == :equal -> false
+      last == :begin -> is_safe?([next | rest], result)
+      result != last -> false
+      true -> is_safe?([next | rest], result)
+    end
+  end
+  defp is_safe?([_], _), do: true
+  defp is_safe?([], _), do: true
+
 end
--- elixir/test/advent_of_code/solution/year_2024/day_02_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_02_test.exs
@@ -6,20 +6,38 @@ defmodule AdventOfCode.Solution.Year2024.Day02Test do
   setup do
     [
       input: """
+      7 6 4 2 1
+      1 2 7 8 9
+      9 7 6 2 1
+      1 3 2 4 5
+      8 6 4 4 1
+      1 3 6 7 9
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "parse", %{input: input} do
+    result = parse(input)
 
-    assert result
+    assert result == [
+             [7, 6, 4, 2, 1],
+             [1, 2, 7, 8, 9],
+             [9, 7, 6, 2, 1],
+             [1, 3, 2, 4, 5],
+             [8, 6, 4, 4, 1],
+             [1, 3, 6, 7, 9]
+           ]
+  end
+
+  test "part1", %{input: input} do
+    result = input |> parse() |> part1()
+
+    assert result == 2
   end
 
   @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
     assert result
   end
