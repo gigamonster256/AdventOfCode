b48b9e5db0490dc207d22ba58dbb72837bd06821
Caleb Norton <n0603919@outlook.com>
2024-12-02 12:12:06 -0600

    Day 2 part 2 not brute force

--- elixir/lib/advent_of_code/solution/year_2024/day_02.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_02.ex
@@ -20,7 +20,7 @@ defmodule AdventOfCode.Solution.Year2024.Day02 do
 
   def part2(parsed_input) do
     parsed_input
-    |> Enum.map(&is_safe_can_fail_once?/1)
+    |> Enum.map(&is_safe_can_fail?/1)
     |> Enum.count(& &1)
   end
 
@@ -33,33 +33,41 @@ defmodule AdventOfCode.Solution.Year2024.Day02 do
     end
   end
 
-  defp is_safe?(list, last \\ :begin)
-
-  defp is_safe?([head, next | rest], last) do
-    result = process_pair(head, next)
-
-    cond do
-      result == :big_step -> false
-      result == :equal -> false
-      last == :begin -> is_safe?([next | rest], result)
-      result != last -> false
-      true -> is_safe?([next | rest], result)
+  defp is_safe?(list) do
+    case is_safe(list) do
+      true -> true
+      {false, _} -> false
     end
   end
 
-  defp is_safe?([_], _), do: true
-  defp is_safe?([], _), do: true
+  defp is_safe(list, index \\ 0, last \\ :begin)
 
-  defp is_safe_can_fail_once?(list) do
-    list
-    |> sublists()
-    |> Enum.any?(&is_safe?/1)
+  defp is_safe([head, next | rest], index, last)
+       when last in [:begin, :increasing, :decreasing] do
+    result = process_pair(head, next)
+
+    cond do
+      result in [:big_step, :equal] -> {false, index}
+      last == :begin -> is_safe([next | rest], index + 1, result)
+      result != last -> {false, index}
+      true -> is_safe([next | rest], index + 1, result)
+    end
   end
 
-  defp sublists(list) do
-    0..(length(list) - 1)
-    |> Enum.map(fn i ->
-      List.delete_at(list, i)
-    end)
+  defp is_safe([_], _index, _last), do: true
+
+  defp is_safe_can_fail?(list, failures \\ 1)
+  defp is_safe_can_fail?(list, 0), do: is_safe?(list)
+
+  defp is_safe_can_fail?(list, failures) do
+    case is_safe(list) do
+      true ->
+        true
+
+      {false, index} ->
+        (index - 1)..(index + 1)
+        |> Stream.filter(&(&1 >= 0))
+        |> Enum.any?(&is_safe_can_fail?(List.delete_at(list, &1), failures - 1))
+    end
   end
 end
