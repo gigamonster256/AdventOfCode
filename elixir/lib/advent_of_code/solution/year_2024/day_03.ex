d3cbc7b1a0c01a41287191815065313aadc35724
Caleb Norton <n0603919@outlook.com>
2024-12-03 15:08:11 -0600

    Elixir: Cleanup 2024 day 3

--- elixir/lib/advent_of_code/solution/year_2024/day_03.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_03.ex
@@ -7,90 +7,101 @@ defmodule AdventOfCode.Solution.Year2024.Day03 do
     do_mul_with_disable(input)
   end
 
-  defp do_mul(input, state \\ :not_in_mul, acc \\ 0) do
-    {rest, state, acc} = mul(input, state, acc)
+  defp do_mul(input, state \\ :not_in_mul, acc \\ 0)
+  defp do_mul(<<>>, _state, acc), do: acc
 
-    case rest do
-      <<>> -> acc
-      _ -> do_mul(rest, state, acc)
-    end
+  defp do_mul(input, state, acc) do
+    {rest, state, acc} = mul(input, state, acc)
+    do_mul(rest, state, acc)
   end
 
-  defp do_mul_with_disable(input, state \\ :not_in_mul, acc \\ 0) do
-    {rest, state, acc} = mul_with_disable(input, state, acc)
+  defp do_mul_with_disable(input, state \\ :not_in_mul, acc \\ 0)
+  defp do_mul_with_disable(<<>>, _state, acc), do: acc
 
-    case rest do
-      <<>> -> acc
-      _ -> do_mul_with_disable(rest, state, acc)
-    end
+  defp do_mul_with_disable(input, state, acc) do
+    {rest, state, acc} = mul_with_disable(input, state, acc)
+    do_mul_with_disable(rest, state, acc)
   end
 
   defguardp is_digit(c) when c in ?0..?9
 
-  defp mul(input, state, acc) do
-    case state do
-      :not_in_mul ->
-        case input do
-          <<"mul(", rest::binary>> ->
-            {rest, {:first_number, []}, acc}
+  defp mul(<<>>, state, acc), do: {<<>>, state, acc}
 
-          <<_::8, rest::binary>> ->
-            {rest, :not_in_mul, acc}
-        end
+  defp mul(input, :not_in_mul, acc) do
+    case input do
+      # begin mul
+      <<"mul(", rest::binary>> ->
+        mul(rest, {:first_number, []}, acc)
 
-      {:first_number, first_number} ->
-        case input do
-          <<c::8, rest::binary>> when is_digit(c) ->
-            {rest, {:first_number, [c | first_number]}, acc}
+      # skip char
+      <<_::8, rest::binary>> ->
+        {rest, :not_in_mul, acc}
+    end
+  end
 
-          <<",", rest::binary>> ->
-            {rest, {:second_number, {first_number, []}}, acc}
+  defp mul(input, {:first_number, first_number}, acc) do
+    case input do
+      # switch to second number
+      <<",", rest::binary>> ->
+        mul(rest, {:second_number, first_number, []}, acc)
 
-          <<_::8, rest::binary>> ->
-            {rest, :not_in_mul, acc}
-        end
+      # accumulate first number
+      <<c::8, rest::binary>> when is_digit(c) ->
+        mul(rest, {:first_number, [c | first_number]}, acc)
 
-      {:second_number, {first_number, second_number}} ->
-        case input do
-          <<c::8, rest::binary>> when is_digit(c) ->
-            {rest, {:second_number, {first_number, [c | second_number]}}, acc}
+      # fail to parse
+      <<_::8, rest::binary>> ->
+        {rest, :not_in_mul, acc}
+    end
+  end
 
-          <<")", rest::binary>> ->
-            mul_result =
-              for(
-                n <- [first_number, second_number],
-                do: n |> Enum.reverse() |> List.to_integer()
-              )
-              |> Enum.reduce(&Kernel.*/2)
+  defp mul(input, {:second_number, first_number, second_number}, acc) do
+    case input do
+      # end mul
+      <<")", rest::binary>> ->
+        mul_result =
+          for(
+            n <- [first_number, second_number],
+            do: n |> Enum.reverse() |> List.to_integer()
+          )
+          |> Enum.reduce(&Kernel.*/2)
 
-            acc = acc + mul_result
-            {rest, :not_in_mul, acc}
+        acc = acc + mul_result
+        {rest, :not_in_mul, acc}
 
-          <<_::8, rest::binary>> ->
-            {rest, :not_in_mul, acc}
-        end
+      # accumulate second number
+      <<c::8, rest::binary>> when is_digit(c) ->
+        mul(rest, {:second_number, first_number, [c | second_number]}, acc)
+
+      # fail to parse
+      <<_::8, rest::binary>> ->
+        {rest, :not_in_mul, acc}
+    end
+  end
+
+  defp mul_with_disable(<<>>, state, acc), do: {<<>>, state, acc}
+
+  defp mul_with_disable(input, :disabled, acc) do
+    case input do
+      # enable
+      <<"do()", rest::binary>> ->
+        mul_with_disable(rest, :not_in_mul, acc)
+
+      # skip char
+      <<_::8, rest::binary>> ->
+        mul_with_disable(rest, :disabled, acc)
     end
   end
 
   defp mul_with_disable(input, state, acc) do
-    case state do
-      :disabled ->
-        case input do
-          <<"do()", rest::binary>> ->
-            mul(rest, :not_in_mul, acc)
+    case input do
+      # disable
+      <<"don't()", rest::binary>> ->
+        mul_with_disable(rest, :disabled, acc)
 
-          <<_::8, rest::binary>> ->
-            {rest, state, acc}
-        end
-
+      # regular mul
       _ ->
-        case input do
-          <<"don't()", rest::binary>> ->
-            {rest, :disabled, acc}
-
-          _ ->
-            mul(input, state, acc)
-        end
+        mul(input, state, acc)
     end
   end
 end
