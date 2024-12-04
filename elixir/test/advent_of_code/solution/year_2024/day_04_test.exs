c90f60ef1c2afe3ce877b3d7f9d72e0f74bfdbec
Caleb Norton <n0603919@outlook.com>
2024-12-04 17:25:34 -0600

    Elixir: Add simplified day 4 tests

--- elixir/test/advent_of_code/solution/year_2024/day_04_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_04_test.exs
@@ -5,7 +5,31 @@ defmodule AdventOfCode.Solution.Year2024.Day04Test do
 
   setup do
     [
-      input: """
+      input1_simple: """
+      ....XXMAS.
+      .SAMXMS...
+      ...S..A...
+      ..A.A.MS.X
+      XMASAMX.MM
+      X.....XA.A
+      S.S.S.S.SS
+      .A.A.A.A.A
+      ..M.M.M.MM
+      .X.X.XMASX
+      """,
+      input2_simple: """
+      .M.S......
+      ..A..MSMS.
+      .M.S.MAA..
+      ..A.ASMSM.
+      .M.S.M....
+      ..........
+      S.S.S.S.S.
+      .A.A.A.A..
+      M.M.M.M.M.
+      ..........
+      """,
+      input_full: """
       MMMSXXMASM
       MSAMXMSMSA
       AMXSXMAAMM
@@ -20,13 +44,25 @@ defmodule AdventOfCode.Solution.Year2024.Day04Test do
     ]
   end
 
-  test "part1", %{input: input} do
+  test "part1_simple", %{input1_simple: input} do
     result = input |> parse() |> part1()
 
     assert result == 18
   end
 
-  test "part2", %{input: input} do
+  test "part1_full", %{input_full: input} do
+    result = input |> parse() |> part1()
+
+    assert result == 18
+  end
+
+  test "part2_simple", %{input2_simple: input} do
+    result = input |> parse() |> part2()
+
+    assert result == 9
+  end
+
+  test "part2_full", %{input_full: input} do
     result = input |> parse() |> part2()
 
     assert result == 9
