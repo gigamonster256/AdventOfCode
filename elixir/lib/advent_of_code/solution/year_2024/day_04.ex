afd5b0ddee3dda4b9f98fe77c963b2bf3e97bb2e
Caleb Norton <n0603919@outlook.com>
2024-12-04 15:51:49 -0600

    Elixir: 2024 day 4

--- elixir/lib/advent_of_code/solution/year_2024/day_04.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_04.ex
@@ -1,7 +1,148 @@
 defmodule AdventOfCode.Solution.Year2024.Day04 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    rows = input |> String.split("\n", trim: true)
+    row_count = Enum.count(rows)
+    col_count = rows |> List.first() |> String.length()
+
+    map =
+      rows
+      |> Enum.with_index()
+      |> Enum.map(fn {row, y} ->
+        row
+        |> String.graphemes()
+        |> Enum.with_index()
+        |> Enum.map(fn {char, x} -> {{x, y}, char} end)
+      end)
+      |> List.flatten()
+      |> Enum.into(%{})
+
+    {map, row_count, col_count}
   end
 
-  def part2(_input) do
+  def part1(input) do
+    {_map, row_count, col_count} = input
+
+    0..(row_count - 1)
+    |> Enum.flat_map(fn y ->
+      0..(col_count - 1)
+      |> Enum.map(fn x -> {x, y} end)
+    end)
+    |> Enum.map(&count_xmas(input, &1))
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    {_map, row_count, col_count} = input
+
+    1..(row_count - 2)
+    |> Enum.flat_map(fn y ->
+      1..(col_count - 2)
+      |> Enum.map(fn x -> {x, y} end)
+    end)
+    |> Enum.filter(&check_for_x_mas(input, &1))
+    |> Enum.count()
+  end
+
+  @directions %{
+    :north => {0, -1},
+    :south => {0, 1},
+    :west => {-1, 0},
+    :east => {1, 0},
+    :north_west => {-1, -1},
+    :north_east => {1, -1},
+    :south_west => {-1, 1},
+    :south_east => {1, 1}
+  }
+
+  @nextchar %{
+    nil => "X",
+    "X" => "M",
+    "M" => "A",
+    "A" => "S"
+  }
+
+  defp is_valid_location?(input, {x, y}) do
+    {_map, row_count, col_count} = input
+    x >= 0 and x < col_count and y >= 0 and y < row_count
+  end
+
+  defp char_at_pos_is?(input, pos, char) do
+    {map, _row_count, _col_count} = input
+    is_valid_location?(input, pos) and Map.get(map, pos) == char
+  end
+
+  defp count_xmas(input, position, last_char \\ nil, direction \\ nil)
+
+  # if we find an S after an A, we have found an XMAS
+  defp count_xmas(input, position, "A", _direction) do
+    if char_at_pos_is?(input, position, "S"), do: 1, else: 0
+  end
+
+  defp count_xmas(input, postion, last_char, direction) do
+    # next char in the nil -> X -> M -> A -> S sequence
+    next_char = Map.get(@nextchar, last_char)
+
+    if char_at_pos_is?(input, postion, next_char),
+      do: count_xmas_in_direction(input, postion, next_char, direction),
+      else: 0
+  end
+
+  # look in all directions
+  defp count_xmas_in_direction(input, {x, y}, last_char, nil) do
+    @directions
+    |> Enum.map(fn {direction, {dx, dy}} -> {direction, {x + dx, y + dy}} end)
+    |> Enum.map(fn {direction, pos} -> count_xmas(input, pos, last_char, direction) end)
+    |> Enum.sum()
+  end
+
+  # continue in the same direction
+  defp count_xmas_in_direction(input, {x, y}, last_char, direction) do
+    {dx, dy} = Map.get(@directions, direction)
+    next_position = {x + dx, y + dy}
+    count_xmas(input, next_position, last_char, direction)
+  end
+
+  defp check_for_x_mas(input, a_position) do
+    char_at_pos_is?(input, a_position, "A") and check_for_ms(input, a_position)
+  end
+
+  # M.S
+  # .x.
+  # M.S
+  @ms_offsets %{
+    {-1, -1} => "M",
+    {-1, 1} => "M",
+    {1, 1} => "S",
+    {1, -1} => "S"
+  }
+
+  defp check_for_ms(input, {a_x, a_y}) do
+    0..3
+    |> Enum.map(&rotation(&1, @ms_offsets))
+    |> Enum.any?(fn offsets ->
+      offsets
+      |> Enum.all?(fn {{dx, dy}, char} ->
+        new_pos = {a_x + dx, a_y + dy}
+        is_valid_location?(input, new_pos) and char_at_pos_is?(input, new_pos, char)
+      end)
+    end)
+  end
+
+  # rotate 90 degrees about the origin
+  defp rotation(0, offsets), do: offsets
+
+  defp rotation(num_rotations, offsets) when num_rotations >= 4,
+    do: rotation(Integer.mod(num_rotations, 4), offsets)
+
+  defp rotation(num_rotations, offsets) do
+    new_offsets =
+      Enum.reduce(offsets, %{}, fn {{x, y}, char}, acc ->
+        Map.put(acc, {y, -x}, char)
+      end)
+
+    rotation(num_rotations - 1, new_offsets)
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_04_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_04_test.exs
@@ -5,22 +5,40 @@ defmodule AdventOfCode.Solution.Year2024.Day04Test do
 
   setup do
     [
+      parse_input: """
+      XM
+      AS
+      """,
       input: """
+      MMMSXXMASM
+      MSAMXMSMSA
+      AMXSXMAAMM
+      MSAMASMSMX
+      XMASAMXAMM
+      XXAMMXXAMA
+      SMSMSASXSS
+      SAXAMASAAA
+      MAMMMXMMMM
+      MXMXAXMASX
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "parse", %{parse_input: input} do
+    result = parse(input)
 
-    assert result
+    assert result == {%{{0, 0} => "X", {1, 0} => "M", {0, 1} => "A", {1, 1} => "S"}, 2, 2}
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part1", %{input: input} do
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 18
+  end
+
+  test "part2", %{input: input} do
+    result = input |> parse() |> part2()
+
+    assert result == 9
   end
 end
