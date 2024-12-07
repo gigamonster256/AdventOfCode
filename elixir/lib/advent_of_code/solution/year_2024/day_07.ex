ff23eb857c9491798c448e366c2b6edaa885ec10
Caleb Norton <n0603919@outlook.com>
2024-12-07 13:06:46 -0600

    Elixir: 2024 day 7

--- elixir/lib/advent_of_code/solution/year_2024/day_07.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_07.ex
@@ -1,7 +1,86 @@
 defmodule AdventOfCode.Solution.Year2024.Day07 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_row/1)
   end
 
-  def part2(_input) do
+  defp parse_row(row) do
+    [total, components] =
+      row
+      |> String.split(": ")
+
+    total =
+      total
+      |> String.to_integer()
+
+    components =
+      components
+      |> String.split()
+      |> Enum.map(&String.to_integer/1)
+
+    {total, components}
+  end
+
+  def part1(input) do
+    input
+    |> Stream.filter(fn {total, components} -> can_result_in?(total, components) end)
+    |> Stream.map(fn {total, _} -> total end)
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    input
+    |> Stream.filter(fn {total, components} -> can_result_in_with_concat?(total, components) end)
+    |> Stream.map(fn {total, _} -> total end)
+    |> Enum.sum()
+  end
+
+  defp can_result_in?(target, components, acc \\ nil)
+  defp can_result_in?(target, [], acc), do: acc == target
+
+  defp can_result_in?(target, components, nil) do
+    args = [target, components, &can_result_in?/3]
+
+    [&can_add_to?/3, &can_multiply_to?/3]
+    |> Enum.any?(&apply(&1, args))
+  end
+
+  defp can_result_in?(target, components, acc) do
+    args = [target, components, &can_result_in?/3, acc]
+
+    [&can_add_to?/4, &can_multiply_to?/4]
+    |> Enum.any?(&apply(&1, args))
+  end
+
+  defp can_result_in_with_concat?(target, components, acc \\ nil)
+  defp can_result_in_with_concat?(target, [], acc), do: acc == target
+
+  defp can_result_in_with_concat?(target, components, nil) do
+    args = [target, components, &can_result_in_with_concat?/3]
+
+    [&can_add_to?/3, &can_multiply_to?/3, &can_concat_to?/3]
+    |> Enum.any?(&apply(&1, args))
+  end
+
+  defp can_result_in_with_concat?(target, components, acc) do
+    args = [target, components, &can_result_in_with_concat?/3, acc]
+
+    [&can_add_to?/4, &can_multiply_to?/4, &can_concat_to?/4]
+    |> Enum.any?(&apply(&1, args))
+  end
+
+  defp can_add_to?(target, [head | tail], callback, acc \\ 0),
+    do: callback.(target, tail, acc + head)
+
+  defp can_multiply_to?(target, [head | tail], callback, acc \\ 1),
+    do: callback.(target, tail, acc * head)
+
+  defp can_concat_to?(target, [head | tail], callback, acc \\ 0) do
+    acc = (Integer.digits(acc) ++ Integer.digits(head)) |> Integer.undigits()
+    callback.(target, tail, acc)
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_07_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_07_test.exs
@@ -6,21 +6,44 @@ defmodule AdventOfCode.Solution.Year2024.Day07Test do
   setup do
     [
       input: """
+      190: 10 19
+      3267: 81 40 27
+      83: 17 5
+      156: 15 6
+      7290: 6 8 6 15
+      161011: 16 10 13
+      192: 17 8 14
+      21037: 9 7 18 13
+      292: 11 6 16 20
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "parse", %{input: input} do
+    result = parse(input)
 
-    assert result
+    assert result == [
+             {190, [10, 19]},
+             {3267, [81, 40, 27]},
+             {83, [17, 5]},
+             {156, [15, 6]},
+             {7290, [6, 8, 6, 15]},
+             {161_011, [16, 10, 13]},
+             {192, [17, 8, 14]},
+             {21037, [9, 7, 18, 13]},
+             {292, [11, 6, 16, 20]}
+           ]
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part1", %{input: input} do
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 3749
+  end
+
+  test "part2", %{input: input} do
+    result = input |> parse() |> part2()
+
+    assert result == 11387
   end
 end
