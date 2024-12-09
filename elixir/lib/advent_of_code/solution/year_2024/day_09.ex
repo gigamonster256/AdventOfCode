5616188aab2199617c0b9e322587290fecc7e7b5
Caleb Norton <n0603919@outlook.com>
2024-12-09 11:22:03 -0600

    Cleanup day 9

--- elixir/lib/advent_of_code/solution/year_2024/day_09.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_09.ex
@@ -13,37 +13,37 @@ defmodule AdventOfCode.Solution.Year2024.Day09 do
       |> Stream.map(&String.to_integer/1)
       |> Enum.with_index()
 
-    spaces =
+    space =
       Stream.drop_every(nums, 2)
       |> Enum.map(&String.to_integer/1)
 
-    {files, spaces}
+    {files, space}
   end
 
   def part1(input) do
-    {files, spaces} = input
+    {files, space} = input
 
     total_length =
       files
-      |> Stream.map(&elem(&1, 0))
+      |> Stream.map(&file_name/1)
       |> Enum.sum()
 
     rev = Enum.reverse(files)
 
     files
-    |> Stream.zip(spaces)
+    |> Stream.zip(space)
     |> Stream.flat_map(&Tuple.to_list/1)
-    |> Stream.transform(rev, fn slot, rev ->
-      case slot do
-        # original data
-        {num, idx} ->
-          # yield idx num times
-          nums = Stream.cycle([idx]) |> Enum.take(num)
+    |> Stream.transform(rev, fn file_or_space, rev ->
+      case file_or_space do
+        # original file
+        {file_len, file_name} ->
+          # yield file_name file_len times
+          nums = List.duplicate(file_name, file_len)
           {nums, rev}
 
         # empty space
-        spaces ->
-          get_spaces_from_rev(rev, spaces)
+        space ->
+          fill_space(rev, space)
       end
     end)
     |> Stream.take(total_length)
@@ -53,31 +53,32 @@ defmodule AdventOfCode.Solution.Year2024.Day09 do
   end
 
   def part2(input) do
-    {files, spaces} = input
+    {files, space} = input
 
     rev = Enum.reverse(files)
 
     files
-    |> Stream.zip(spaces)
+    |> Stream.zip(space)
     |> Stream.flat_map(&Tuple.to_list/1)
-    |> Stream.transform({rev, MapSet.new([])}, fn slot, {rev, eliminated} ->
-      case slot do
-        # original data
-        {num, idx} ->
+    |> Stream.transform({rev, MapSet.new([])}, fn file_or_space, {rev, defragged} ->
+      case file_or_space do
+        # original file
+        {file_len, file_name} ->
           # number was already compacted
-          if MapSet.member?(eliminated, idx) do
+          if MapSet.member?(defragged, file_name),
             # try to fill its space
-            get_spaces_from_rev_no_frag(rev, num, eliminated)
-          else
-            # yield idx num times
-            nums = Stream.cycle([idx]) |> Enum.take(num)
-            eliminated = MapSet.put(eliminated, idx)
-            {nums, {rev, eliminated}}
-          end
+            do: fill_space_no_frag(rev, file_len, defragged),
+            else:
+              (
+                # yield file_name file_len times
+                nums = List.duplicate(file_name, file_len)
+                defragged = MapSet.put(defragged, file_name)
+                {nums, {rev, defragged}}
+              )
 
         # empty space
-        spaces ->
-          get_spaces_from_rev_no_frag(rev, spaces, eliminated)
+        space ->
+          fill_space_no_frag(rev, space, defragged)
       end
     end)
     |> Stream.with_index()
@@ -85,70 +86,77 @@ defmodule AdventOfCode.Solution.Year2024.Day09 do
     |> Enum.sum()
   end
 
-  defp get_spaces_from_rev(rev, spaces, acc \\ []) do
-    # we have num amount of idx and we need spaces amount of spaces
-    [{num, idx} | new_rev] = rev
+  defp file_name({file_len, _file_name}), do: file_len
+
+  defp fill_space(rev, space, acc \\ []) do
+    # we have file_len amount of file_name and we need space amount of space
+    [{file_len, file_name} | rev] = rev
 
     cond do
-      # we have enough nums to fulfill the spaces
-      num > spaces ->
-        # take spaces amount of idx
-        nums = Stream.cycle([idx]) |> Enum.take(spaces)
-        leftover = num - spaces
-        {acc ++ nums, [{leftover, idx} | new_rev]}
+      # we have enough nums to fulfill the space
+      file_len > space ->
+        # take space amount of file_name
+        acc = acc ++ List.duplicate(file_name, space)
+        leftover = file_len - space
 
-      # we have exactly enough nums to fulfill the spaces
-      num == spaces ->
-        nums = Stream.cycle([idx]) |> Enum.take(spaces)
-        {acc ++ nums, new_rev}
+        {acc, [{leftover, file_name} | rev]}
+
+      # we have exactly enough nums to fulfill the space
+      file_len == space ->
+        acc = acc ++ List.duplicate(file_name, space)
+
+        {acc, rev}
 
       # we need to take all the nums and some from the next
-      num < spaces ->
-        nums = Stream.cycle([idx]) |> Enum.take(num)
-        get_spaces_from_rev(new_rev, spaces - num, acc ++ nums)
+      file_len < space ->
+        acc = acc ++ List.duplicate(file_name, file_len)
+        space = space - file_len
+
+        fill_space(rev, space, acc)
     end
   end
 
-  defp get_spaces_from_rev_no_frag(rev, spaces, eliminated, acc \\ {[], []})
+  defp fill_space_no_frag(rev, space, defragged, acc \\ {[], []})
 
-  defp get_spaces_from_rev_no_frag([], spaces, eliminated, {acc, rev_left}) do
-    # fill in the rest of the spaces with 0
-    acc = acc ++ (Stream.cycle([0]) |> Enum.take(spaces))
-    rev_left = rev_left |> Enum.reverse()
+  defp fill_space_no_frag([], space, defragged, {acc, too_big}) do
+    # fill in the rest of the space with 0
+    acc = acc ++ List.duplicate(0, space)
+    rev = too_big |> Enum.reverse()
 
-    {acc, {rev_left, eliminated}}
+    {acc, {rev, defragged}}
   end
 
-  defp get_spaces_from_rev_no_frag(rev, spaces, eliminated, {acc, rev_left}) do
-    # we have num amount of idx and we need spaces amount of spaces
-    [{num, idx} | new_rev] = rev
+  defp fill_space_no_frag(rev, space, defragged, {acc, too_big}) do
+    # we have file_len amount of file_name and we need space amount of space
+    [file | rev] = rev
+    {file_len, file_name} = file
 
     cond do
-      # if we have eliminated this idx, skip
-      MapSet.member?(eliminated, idx) ->
-        get_spaces_from_rev_no_frag(new_rev, spaces, eliminated, {acc, rev_left})
+      # if we have defragged this file_name, skip
+      MapSet.member?(defragged, file_name) ->
+        fill_space_no_frag(rev, space, defragged, {acc, too_big})
 
-      # we have too many nums... try the next number
-      num > spaces ->
-        rev_left = [{num, idx} | rev_left]
-        # do not fragment the list (keep spaces)
-        get_spaces_from_rev_no_frag(new_rev, spaces, eliminated, {acc, rev_left})
+      # file is too big to fit in the space
+      file_len > space ->
+        too_big = [file | too_big]
+        # do not fragment the list (keep space)
+        fill_space_no_frag(rev, space, defragged, {acc, too_big})
 
-      # we have exactly enough nums to fulfill the spaces
-      num == spaces ->
-        acc = acc ++ (Stream.cycle([idx]) |> Enum.take(spaces))
-        eliminated = MapSet.put(eliminated, idx)
-        rev_left = (rev_left |> Enum.reverse()) ++ new_rev
+      # file fits exactly in the space
+      file_len == space ->
+        acc = acc ++ List.duplicate(file_name, space)
+        defragged = MapSet.put(defragged, file_name)
+        rev = Enum.reverse(too_big) ++ rev
 
-        {acc, {rev_left, eliminated}}
+        {acc, {rev, defragged}}
 
-      # we need to take all the nums and some from the next
-      num < spaces ->
-        spaces = spaces - num
-        eliminated = MapSet.put(eliminated, idx)
-        acc = acc ++ (Stream.cycle([idx]) |> Enum.take(num))
+      # file fits in the space and we have space left
+      file_len < space ->
+        space = space - file_len
+        defragged = MapSet.put(defragged, file_name)
+        acc = acc ++ List.duplicate(file_name, file_len)
 
-        get_spaces_from_rev_no_frag(new_rev, spaces, eliminated, {acc, rev_left})
+        fill_space_no_frag(rev, space, defragged, {acc, too_big})
     end
   end
 end
