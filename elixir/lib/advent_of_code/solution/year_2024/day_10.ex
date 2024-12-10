ab52451459a21854d33c9fa3a5bf292bd8af683e
Caleb Norton <n0603919@outlook.com>
2024-12-10 11:40:04 -0600

    Elixir: 2024 day 10

--- elixir/lib/advent_of_code/solution/year_2024/day_10.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_10.ex
@@ -1,7 +1,63 @@
 defmodule AdventOfCode.Solution.Year2024.Day10 do
-  def part1(_input) do
+  use AdventOfCode.Solution.GridMap
+
+  # handle examples with . in the input
+  def mapper("."), do: nil
+  def mapper(char),do: String.to_integer(char)
+
+  def part1(input) do
+    trailheads(input)
+    |> Enum.map(&score(input, &1))
+    |> Enum.sum()
   end
 
-  def part2(_input) do
+  def part2(input) do
+    trailheads(input)
+    |> Enum.map(&rating(input, &1))
+    |> Enum.sum()
+  end
+
+  @directions %{
+    :up => {0, -1},
+    :down => {0, 1},
+    :right => {1, 0},
+    :left => {-1, 0}
+  }
+
+  defp move({x, y}, {dx, dy}), do: {x + dx, y + dy}
+  defp move({x, y}, dir) when is_atom(dir), do: move({x, y}, Map.get(@directions, dir))
+  defp move_cardinal(pos), do: @directions |> Map.keys() |> Enum.map(&move(pos, &1))
+
+  defp trailheads(input), do: find(input, 0)
+
+  defp trailhead_endings(input, pos) do
+    case get(input, pos) do
+      9 ->
+        [pos]
+
+      current ->
+        move_cardinal(pos)
+        |> Enum.filter(&char_at_pos_is?(input, &1, current + 1))
+        |> Enum.flat_map(&trailhead_endings(input, &1))
+        |> Enum.uniq()
+    end
+  end
+
+  defp score(input, pos) do
+    trailhead_endings(input, pos)
+    |> Enum.count()
+  end
+
+  defp rating(input, pos) do
+    case get(input, pos) do
+      9 ->
+        1
+
+      current ->
+        move_cardinal(pos)
+        |> Enum.filter(&char_at_pos_is?(input, &1, current + 1))
+        |> Enum.map(&rating(input, &1))
+        |> Enum.sum()
+    end
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_10_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_10_test.exs
@@ -6,21 +6,41 @@ defmodule AdventOfCode.Solution.Year2024.Day10Test do
   setup do
     [
       input: """
+      89010123
+      78121874
+      87430965
+      96549874
+      45678903
+      32019012
+      01329801
+      10456732
+      """,
+      input2: """
+      012345
+      123456
+      234567
+      345678
+      4.6789
+      56789.
       """
     ]
   end
 
-  @tag :skip
   test "part1", %{input: input} do
-    result = part1(input)
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 36
   end
 
-  @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
-    assert result
+    assert result == 81
+  end
+
+  test "part2 with input2", %{input2: input} do
+    result = input |> parse() |> part2()
+
+    assert result == 227
   end
 end
