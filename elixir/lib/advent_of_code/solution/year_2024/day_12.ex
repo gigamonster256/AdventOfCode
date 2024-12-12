f1ec7bfb0e4708a7c213e08cc4bdc2bfae9cd581
Caleb Norton <n0603919@outlook.com>
2024-12-11 23:49:16 -0600

    Calculate perimiter?

--- elixir/lib/advent_of_code/solution/year_2024/day_12.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_12.ex
@@ -1,7 +1,73 @@
 defmodule AdventOfCode.Solution.Year2024.Day12 do
-  def part1(_input) do
+  use AdventOfCode.Solution.GridMap
+
+  def part1(input) do
+    input
+    |> uniq()
+    |> Enum.flat_map(&regions(input, &1))
+    |> Enum.map(&price/1)
+    |> Enum.sum()
   end
 
   def part2(_input) do
+  end
+
+  @directions %{
+    up: {0, -1},
+    down: {0, 1},
+    left: {-1, 0},
+    right: {1, 0}
+  }
+
+  defp neighbor_positions(input, pos) do
+    {x, y} = pos
+
+    @directions
+    |> Map.values()
+    |> Enum.map(fn {dx, dy} -> {x + dx, y + dy} end)
+    |> Enum.filter(&is_valid_position?(input, &1))
+  end
+
+  defp regions(input, char) do
+    all_positions = find(input, char)
+
+    Stream.unfold(all_positions, fn positions ->
+      if Enum.empty?(positions),
+        do: nil,
+        else:
+          (
+            [start | positions] = positions
+            {region, remaining} = greedy_region(input, start, MapSet.new(positions))
+            {region, Enum.to_list(remaining)}
+          )
+    end)
+    |> Enum.to_list()
+    |> IO.inspect()
+  end
+
+  defp greedy_region(input, pos, positions, region \\ MapSet.new())
+
+  defp greedy_region(input, pos, positions, region) do
+    region = MapSet.put(region, pos)
+    neighbors = neighbor_positions(input, pos)
+
+    neighbors
+    |> Enum.filter(&MapSet.member?(positions, &1))
+    |> Enum.reduce({region, positions}, fn neighbor, {region, positions} ->
+      if MapSet.member?(positions, neighbor),
+        do: greedy_region(input, neighbor, MapSet.delete(positions, neighbor), region),
+        else: {region, positions}
+    end)
+  end
+
+  defp price(region) do
+    area(region) * perimiter(region)
+  end
+
+  defp area(region), do: MapSet.size(region)
+
+  defp perimiter(region) do
+    # wut
+    1
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_12_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_12_test.exs
@@ -5,21 +5,58 @@ defmodule AdventOfCode.Solution.Year2024.Day12Test do
 
   setup do
     [
-      input: """
+      input1: """
+      AAAA
+      BBCD
+      BBCC
+      EEEC
+      """,
+      input2: """
+      OOOOO
+      OXOXO
+      OOOOO
+      OXOXO
+      OOOOO
+      """,
+      input3: """
+      RRRRIICCFF
+      RRRRIICCCF
+      VVRRRCCFFF
+      VVRCCCJFFF
+      VVVVCJJCFE
+      VVIVCCJJEE
+      VVIIICJJEE
+      MIIIIIJJEE
+      MIIISIJEEE
+      MMMISSJEEE
       """
     ]
   end
 
   @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "part1 input 1", %{input1: input} do
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 140
+  end
+
+  # @tag :skip
+  test "part1 input 2", %{input2: input} do
+    result = input |> parse() |> part1()
+
+    assert result == 772
+  end
+
+  @tag :skip
+  test "part1 input 3", %{input3: input} do
+    result = input |> parse() |> part1()
+
+    assert result == 1930
   end
 
   @tag :skip
   test "part2", %{input: input} do
-    result = part2(input)
+    result = input |> parse() |> part2()
 
     assert result
   end
