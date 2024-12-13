44b663292345e4d73999bc947ab2c61c123f9de3
Caleb Norton <n0603919@outlook.com>
2024-12-13 15:01:36 -0600

    Elixir: 2024 day 13

--- elixir/lib/advent_of_code/solution/year_2024/day_13.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_13.ex
@@ -1,7 +1,73 @@
 defmodule AdventOfCode.Solution.Year2024.Day13 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    input
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(&parse_machine/1)
   end
 
-  def part2(_input) do
+  defp parse_machine(machine) do
+    machine
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
+    |> List.to_tuple()
+  end
+
+  defp parse_line(line) do
+    line
+    |> String.split(": ")
+    |> Enum.drop(1)
+    |> then(&parse_coordinates/1)
+  end
+
+  defp parse_coordinates([coordinates]) do
+    coordinates
+    |> String.replace(~r/[XY=+]/, "")
+    |> String.split(", ")
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  def part1(input) do
+    input
+    |> Enum.map(&price/1)
+    |> Enum.sum()
+  end
+
+  def part2(input) do
+    input
+    |> Enum.map(&part1_machine_to_part2_machine/1)
+    |> part1()
+  end
+
+  defp part1_machine_to_part2_machine(machine) do
+    {a, b, prize} = machine
+    {px, py} = prize
+    prize = {px + 10_000_000_000_000, py + 10_000_000_000_000}
+
+    {a, b, prize}
+  end
+
+  defp presses({x1, y1}, {x2, y2}, {px, py}, button) do
+    {numerator, denominator} =
+      case button do
+        :a -> {x2 * py - y2 * px, x2 * y1 - y2 * x1}
+        :b -> {x1 * py - y1 * px, x1 * y2 - y1 * x2}
+      end
+
+    if rem(numerator, denominator) == 0,
+      do: {:ok, div(numerator, denominator)},
+      else: :error
+  end
+
+  defp price(machine) do
+    {a, b, prize} = machine
+
+    with {:ok, a_presses} <- presses(a, b, prize, :a),
+         {:ok, b_presses} <- presses(a, b, prize, :b),
+         do: 3 * a_presses + b_presses,
+         else: (_ -> 0)
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_13_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_13_test.exs
@@ -6,21 +6,45 @@ defmodule AdventOfCode.Solution.Year2024.Day13Test do
   setup do
     [
       input: """
+      Button A: X+94, Y+34
+      Button B: X+22, Y+67
+      Prize: X=8400, Y=5400
+
+      Button A: X+26, Y+66
+      Button B: X+67, Y+21
+      Prize: X=12748, Y=12176
+
+      Button A: X+17, Y+86
+      Button B: X+84, Y+37
+      Prize: X=7870, Y=6450
+
+      Button A: X+69, Y+23
+      Button B: X+27, Y+71
+      Prize: X=18641, Y=10279
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "parse", %{input: input} do
+    result = parse(input)
 
-    assert result
+    assert result == [
+             {{94, 34}, {22, 67}, {8400, 5400}},
+             {{26, 66}, {67, 21}, {12748, 12176}},
+             {{17, 86}, {84, 37}, {7870, 6450}},
+             {{69, 23}, {27, 71}, {18641, 10279}}
+           ]
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part1", %{input: input} do
+    result = input |> parse() |> part1()
 
-    assert result
+    assert result == 480
+  end
+
+  test "part2", %{input: input} do
+    result = input |> parse() |> part2()
+
+    assert result == 875_318_608_908
   end
 end
