77969c356a06fbe2b75cc1f842fca5416110898c
Caleb Norton <n0603919@outlook.com>
2024-12-14 14:57:41 -0600

    Elixir: 2024 day 14

--- elixir/lib/advent_of_code/solution/year_2024/day_14.ex
+++ elixir/lib/advent_of_code/solution/year_2024/day_14.ex
@@ -1,7 +1,157 @@
 defmodule AdventOfCode.Solution.Year2024.Day14 do
-  def part1(_input) do
+  use AdventOfCode.Solution.SharedParse
+
+  @impl true
+  def parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&parse_line/1)
   end
 
-  def part2(_input) do
+  defp parse_line(line) do
+    line
+    |> String.split(" ", trim: true)
+    |> Enum.flat_map(&parse_pair/1)
+    |> List.to_tuple()
+  end
+
+  defp parse_pair(pair) do
+    pair
+    |> String.replace(~r/[pv=]/, "")
+    |> String.split(" ")
+    |> Enum.map(&parse_nums/1)
+  end
+
+  defp parse_nums(num) do
+    num
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+    |> List.to_tuple()
+  end
+
+  def part1(input, bounds \\ {101, 103}) do
+    input
+    |> robots_after(bounds, 100)
+    |> partition(bounds)
+    |> Map.values()
+    |> Enum.map(&Enum.count(&1))
+    |> Enum.product()
+  end
+
+  # I assumed for part 2 that the chistmas tree would be in the center of the screen horizontally and have
+  # left right symmetry. Looking at other people's solutions, I see that this is not the case.
+  # but this code worked for me!
+  def part2(input, bounds \\ {101, 103}, print \\ false) do
+    Stream.iterate(0, &(&1 + 1))
+    # get symmetrical differences for each second
+    # (how different the left and right sides are)
+    |> Stream.map(fn seconds ->
+      input
+      |> robots_after(bounds, seconds)
+      |> partition(bounds)
+      |> symmetric_difference(bounds)
+    end)
+    |> Stream.with_index()
+    # assume it's in the first 10000 seconds
+    |> Stream.take(10000)
+    # find the minimum symmetric difference
+    |> Enum.min_by(&elem(&1, 0))
+    |> elem(1)
+    |> tap(fn seconds ->
+      if print do
+        IO.puts("Seconds: #{seconds}")
+
+        input
+        |> robots_after(bounds, seconds)
+        |> tap(&print_robots(&1, bounds))
+        |> partition(bounds)
+        |> symmetric_difference(bounds)
+        |> IO.inspect(label: "Symmetric Difference")
+      end
+    end)
+  end
+
+  defp robots_after(input, bounds, seconds) do
+    input
+    |> Enum.map(&position_after(&1, bounds, seconds))
+  end
+
+  defp position_after({pos, vel}, bounds, seconds) do
+    {x, y} = pos
+    {vx, vy} = vel
+    {bx, by} = bounds
+
+    x = (((x + vx * seconds) |> rem(bx)) + bx) |> rem(bx)
+    y = (((y + vy * seconds) |> rem(by)) + by) |> rem(by)
+    {x, y}
+  end
+
+  defp partition(positions, bounds) do
+    {bx, by} = bounds
+    midx = div(bx - 1, 2)
+    midy = div(by - 1, 2)
+
+    default = %{
+      tl: [],
+      tr: [],
+      bl: [],
+      br: []
+    }
+
+    Enum.reduce(positions, default, fn {x, y} = pos, acc ->
+      cond do
+        x < midx and y < midy -> Map.update!(acc, :tl, &[pos | &1])
+        x > midx and y < midy -> Map.update!(acc, :tr, &[pos | &1])
+        x < midx and y > midy -> Map.update!(acc, :bl, &[pos | &1])
+        x > midx and y > midy -> Map.update!(acc, :br, &[pos | &1])
+        true -> acc
+      end
+    end)
+  end
+
+  defp symmetric_difference(partitions, bounds) do
+    tl = Map.fetch!(partitions, :tl) |> MapSet.new()
+    tr = Map.fetch!(partitions, :tr)
+
+    bl = Map.fetch!(partitions, :bl) |> MapSet.new()
+    br = Map.fetch!(partitions, :br)
+
+    reversed_tr = reverse_along_midy(tr, bounds) |> MapSet.new()
+    reversed_br = reverse_along_midy(br, bounds) |> MapSet.new()
+
+    top_diff = MapSet.difference(tl, reversed_tr) |> MapSet.size()
+    bottom_diff = MapSet.difference(bl, reversed_br) |> MapSet.size()
+
+    top_diff + bottom_diff
+  end
+
+  defp reverse_along_midy(partition, bounds) do
+    {bx, _by} = bounds
+
+    Enum.map(partition, fn {x, y} ->
+      x = bx - x - 1
+      {x, y}
+    end)
+  end
+
+  defp print_robots(positions, bounds) do
+    positions = MapSet.new(positions)
+    {bx, by} = bounds
+
+    0..(by - 1)
+    |> Enum.map(fn y ->
+      0..(bx - 1)
+      |> Enum.map(fn x ->
+        if MapSet.member?(positions, {x, y}) do
+          "#"
+        else
+          "."
+        end
+      end)
+      |> Enum.join()
+    end)
+    |> Enum.join("\n")
+    |> then(&(&1 <> "\n"))
+    |> IO.puts()
   end
 end
--- elixir/test/advent_of_code/solution/year_2024/day_14_test.exs
+++ elixir/test/advent_of_code/solution/year_2024/day_14_test.exs
@@ -6,21 +6,52 @@ defmodule AdventOfCode.Solution.Year2024.Day14Test do
   setup do
     [
       input: """
+      p=0,4 v=3,-3
+      p=6,3 v=-1,-3
+      p=10,3 v=-1,2
+      p=2,0 v=2,-1
+      p=0,0 v=1,3
+      p=3,0 v=-2,-2
+      p=7,6 v=-1,-3
+      p=3,0 v=-1,-2
+      p=9,3 v=2,3
+      p=7,3 v=-1,2
+      p=2,4 v=2,-3
+      p=9,5 v=-3,-3
       """
     ]
   end
 
-  @tag :skip
-  test "part1", %{input: input} do
-    result = part1(input)
+  test "parse", %{input: input} do
+    result = parse(input)
 
-    assert result
+    assert result == [
+             {{0, 4}, {3, -3}},
+             {{6, 3}, {-1, -3}},
+             {{10, 3}, {-1, 2}},
+             {{2, 0}, {2, -1}},
+             {{0, 0}, {1, 3}},
+             {{3, 0}, {-2, -2}},
+             {{7, 6}, {-1, -3}},
+             {{3, 0}, {-1, -2}},
+             {{9, 3}, {2, 3}},
+             {{7, 3}, {-1, 2}},
+             {{2, 4}, {2, -3}},
+             {{9, 5}, {-3, -3}}
+           ]
   end
 
-  @tag :skip
-  test "part2", %{input: input} do
-    result = part2(input)
+  test "part1", %{input: input} do
+    bounds = {11, 7}
+    result = input |> parse() |> part1(bounds)
 
-    assert result
+    assert result == 12
+  end
+
+  test "part2", %{input: input} do
+    bounds = {11, 7}
+    result = input |> parse() |> part2(bounds)
+
+    assert result == 31
   end
 end
